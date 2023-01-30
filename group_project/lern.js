bb8533338adfa1f2180b8f52d7c655a38d0a2e42
Shivangi kothe <106828782+shivusharma1234@users.noreply.github.com>
2023-01-30 04:20:47 -0800

    Xtan-technogig

---
+++ group_project/lern.js
@@ -0,0 +1,14 @@
+// function avg(a,b){
+//     return (a+b)/2;
+// }
+// c1=avg(4,6);
+// c2=avg(14,24);
+// console.log(c1,c2);
+// var age=16;
+// if(age>=18){
+//     console.log("you can vote")
+// }
+// else{
+//     console.log("you can not vote")
+// }
+<button id="click" onclick="clicked()">Click Me</button>
